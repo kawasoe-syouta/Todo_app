12d5ffede45e3c34cfc9fb9d13919e41db66ccd0
kawasoe-syouta <d6hyj3dyr@gmail.com>
2020-09-06 23:35:40 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root "home#index"
   resources :tasks do
     resource :importants, only: [:create, :destroy]
   end

5d72a79549a05a38e636bc5b027bf7c02bdae429
kawasoe-syouta <d6hyj3dyr@gmail.com>
2020-09-09 17:21:38 +0900

    Revert "commit"
    This reverts commit 6f99e46d1b1758c2c4ae3e7a90127918f0cbd0f7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :searches, only: [:index]
   resources :tasks do
     resource :importants, only: [:create, :destroy]
   end
